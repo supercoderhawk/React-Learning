772cb379d8158e52075233b20f51d8b6719cdeda
SuperCoderX <xyb930826@gmail.com>
2016-05-28 19:05:33 +0800

    fix bug

--- test2/server.js
+++ test2/server.js
@@ -12,7 +12,7 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(function(){
+app.use(function(req, res, next){
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Cache-Control', 'no-cache');
     next();
